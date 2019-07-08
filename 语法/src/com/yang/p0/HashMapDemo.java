beb3319f6749d688bd1a41e9f3a203a286f2944c
11660 <https://github.com/KernelEvil/HANSHUAI.git>
2019-07-08 20:10:45 +0800

    2019.9.18日提交

--- 语法/src/com/yang/p0/HashMapDemo.java
+++ 语法/src/com/yang/p0/HashMapDemo.java
@@ -1,4 +1,15 @@
 package com.yang.p0;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HashMapDemo {
+    public static void main(String argv[]){
+        Map<Integer,String> m = new HashMap<Integer, String>();
+        m.put(1,"张三");
+        m.put(2,"李四");
+
+        String name = m.get(1);
+        System.out.println(name);
+    }
 }
