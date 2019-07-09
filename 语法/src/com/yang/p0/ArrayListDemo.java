cfb59b314c7ef82a775f47494f7c161bdae761df
11660 <https://github.com/KernelEvil/HANSHUAI.git>
2019-07-10 07:55:28 +0800

    2019.7.10日提交

--- 语法/src/com/yang/p0/ArrayListDemo.java
+++ 语法/src/com/yang/p0/ArrayListDemo.java
@@ -1,4 +1,13 @@
 package com.yang.p0;
 
+import java.util.ArrayList;
+
 public class ArrayListDemo {
+    public static void main(String argv[]){
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        System.out.println(list);
+    }
 }
---
+++ 语法/src/com/yang/p0/包装类.java
@@ -0,0 +1,7 @@
+package com.yang.p0;
+
+public class 包装类 {
+    public static void main(String argv[]){
+
+    }
+}
